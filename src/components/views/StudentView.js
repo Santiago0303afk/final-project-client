a71f7c5e4408bece3fa06534d810455aa5aa422c
Santiago0303afk <santiago.cadme71@myhunter.cuny.edu>
2025-05-15 00:28:57 -0400

    StudentView.js

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,17 +4,21 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
-const StudentView = (props) => {
-  const { student } = props;
+const StudentView = ({ student }) => {
+  if (!student) {
+    return <div><h2>Loading student...</h2></div>;
+  }
 
-  // Render a single Student view 
   return (
     <div>
-      <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus.name}</h3>
+      <h1>{student.firstname} {student.lastname}</h1>
+      <h3>
+        {student.campus
+          ? `Campus: ${student.campus.name}`
+          : 'Not enrolled in any campus'}
+      </h3>
     </div>
   );
-
 };
 
 export default StudentView;
