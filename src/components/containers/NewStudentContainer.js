56f35f193bf737781ac27f7587f51bda94191894
Santiago0303afk <santiago.cadme71@myhunter.cuny.edu>
2025-05-15 00:09:48 -0400

    NewStudentContainer.js

--- src/components/containers/NewStudentContainer.js
+++ src/components/containers/NewStudentContainer.js
@@ -14,83 +14,80 @@ import NewStudentView from '../views/NewStudentView';
 import { addStudentThunk } from '../../store/thunks';
 
 class NewStudentContainer extends Component {
-  // Initialize state
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      firstname: "", 
-      lastname: "", 
-      campusId: null, 
-      redirect: false, 
+      firstname: "",
+      lastname: "",
+      campusId: null,
+      redirect: false,
       redirectId: null
     };
   }
 
-  // Capture input data when it is entered
   handleChange = event => {
     this.setState({
       [event.target.name]: event.target.value
     });
   }
 
-  // Take action after user click the submit button
   handleSubmit = async event => {
-    event.preventDefault();  // Prevent browser reload/refresh after submit.
+    event.preventDefault();
 
-    let student = {
-        firstname: this.state.firstname,
-        lastname: this.state.lastname,
-        campusId: this.state.campusId
+    const student = {
+      firstname: this.state.firstname,
+      lastname: this.state.lastname,
+      campusId: this.state.campusId
     };
-    
-    // Add new student in back-end database
-    let newStudent = await this.props.addStudent(student);
 
-    // Update state, and trigger redirect to show the new student
-    this.setState({
-      firstname: "", 
-      lastname: "", 
-      campusId: null, 
-      redirect: true, 
-      redirectId: newStudent.id
-    });
+    try {
+      const newStudent = await this.props.addStudent(student);
+
+      // Defensive check in case the thunk doesn't return expected data
+      if (!newStudent || !newStudent.id) {
+        console.error("Student creation failed or no ID returned:", newStudent);
+        alert("Failed to create student. Please check your input or try again.");
+        return;
+      }
+
+      this.setState({
+        firstname: "",
+        lastname: "",
+        campusId: null,
+        redirect: true,
+        redirectId: newStudent.id
+      });
+    } catch (error) {
+      console.error("Error while submitting new student:", error);
+      alert("An error occurred while submitting the form.");
+    }
   }
 
-  // Unmount when the component is being removed from the DOM:
   componentWillUnmount() {
-      this.setState({redirect: false, redirectId: null});
+    this.setState({ redirect: false, redirectId: null });
   }
 
-  // Render new student input form
   render() {
-    // Redirect to new student's page after submit
-    if(this.state.redirect) {
-      return (<Redirect to={`/student/${this.state.redirectId}`}/>)
+    if (this.state.redirect) {
+      return <Redirect to={`/student/${this.state.redirectId}`} />;
     }
 
-    // Display the input form via the corresponding View component
     return (
       <div>
         <Header />
-        <NewStudentView 
-          handleChange = {this.handleChange} 
-          handleSubmit={this.handleSubmit}      
+        <NewStudentView
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
         />
-      </div>          
+      </div>
     );
   }
 }
 
-// The following input argument is passed to the "connect" function used by "NewStudentContainer" component to connect to Redux Store.
-// The "mapDispatch" argument is used to dispatch Action (Redux Thunk) to Redux Store.
-// The "mapDispatch" calls the specific Thunk to dispatch its action. The "dispatch" is a function of Redux Store.
 const mapDispatch = (dispatch) => {
-    return({
-        addStudent: (student) => dispatch(addStudentThunk(student)),
-    })
-}
+  return {
+    addStudent: (student) => dispatch(addStudentThunk(student)),
+  };
+};
 
-// Export store-connected container by default
-// NewStudentContainer uses "connect" function to connect to Redux Store and to read values from the Store 
-// (and re-read the values when the Store State updates).
 export default connect(null, mapDispatch)(NewStudentContainer);
