744eb3fefcb252aebf22d8e2e70d6d7df46d0d53
Santiago0303afk <santiago.cadme71@myhunter.cuny.edu>
2025-05-16 12:37:03 -0400

    AllCampusesContainer.js

--- src/components/containers/AllCampusesContainer.js
+++ src/components/containers/AllCampusesContainer.js
@@ -5,86 +5,56 @@ The Container component is responsible for stateful logic and data fetching, and
 passes data (if any) as props to the corresponding View component.
 If needed, it also defines the component's "connect" function.
 ================================================== */
-import React, { Component } from "react";
+import Header from './Header';
+import { Component } from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { addCampusThunk } from "../../store/thunks";
-import { AddCampusView } from "../views";
-import Header from "./Header";
+import { fetchAllCampusesThunk } from "../../store/thunks";
+import { AllCampusesView } from "../views";
 
-class AddCampusContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      address: "",
-      description: "",
-      imageUrl: "",
-      errors: {}
-    };
+class AllCampusesContainer extends Component {
+  // Get all campuses data from back-end database
+  componentDidMount() {
+    console.log(this.props);
+    this.props.fetchAllCampuses();
   }
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
-
-  validate = () => {
-    const errors = {};
-    if (!this.state.name.trim()) errors.name = "Name is required.";
-    if (!this.state.address.trim()) errors.address = "Address is required.";
-    return errors;
-  };
-
-  handleSubmit = async (event) => {
-    event.preventDefault();
-    const errors = this.validate();
-    if (Object.keys(errors).length > 0) {
-      this.setState({ errors });
-      return;
-    }
-
-    const newCampus = {
-      name: this.state.name,
-      address: this.state.address,
-      description: this.state.description,
-      imageUrl: this.state.imageUrl || undefined
-    };
-
-    const createdCampus = await this.props.addCampus(newCampus);
-    this.setState({
-      name: "",
-      address: "",
-      description: "",
-      imageUrl: "",
-      errors: {}
-    });
-
-    this.props.history.push("/campuses");
-  };
-
+  // Render All Campuses view by passing all campuses data as props to the corresponding View component
   render() {
     return (
       <div>
         <Header />
-        <AddCampusView
-          name={this.state.name}
-          address={this.state.address}
-          description={this.state.description}
-          imageUrl={this.state.imageUrl}
-          errors={this.state.errors}
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit}
+        <AllCampusesView
+          allCampuses={this.props.allCampuses}
         />
       </div>
     );
   }
 }
 
+// 1. The "mapState" argument specifies the data from Redux Store that the component needs.
+// The "mapState" is called when the Store State changes, and it returns a data object of "allCampuses".
+// The following 2 input arguments are passed to the "connect" function used by "AllCampusesContainer" component to connect to Redux Store.
+const mapState = (state) => {
+  return {
+    allCampuses: state.allCampuses,  // Get the State object from Reducer "allCampuses"
+  };
+};  
+// 2. The "mapDispatch" argument is used to dispatch Action (Redux Thunk) to Redux Store.
+// The "mapDispatch" calls the specific Thunk to dispatch its action. The "dispatch" is a function of Redux Store.
 const mapDispatch = (dispatch) => {
   return {
-    addCampus: (campus) => dispatch(addCampusThunk(campus))
+    fetchAllCampuses: () => dispatch(fetchAllCampusesThunk()),
   };
 };
 
-export default connect(null, mapDispatch)(AddCampusContainer);
+// Type check props;
+AllCampusesContainer.propTypes = {
+  allCampuses: PropTypes.array.isRequired,
+  fetchAllCampuses: PropTypes.func.isRequired,
+};
+
+// Export store-connected container by default
+// AllCampusesContainer uses "connect" function to connect to Redux Store and to read values from the Store 
+// (and re-read the values when the Store State updates).
+export default connect(mapState, mapDispatch)(AllCampusesContainer);
