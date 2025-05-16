1e9a99eebf85298dc71904c829a92c0570114467
Santiago0303afk <santiago.cadme71@myhunter.cuny.edu>
2025-05-16 00:14:59 -0400

    index.js

--- src/components/containers/index.js
+++ src/components/containers/index.js
@@ -7,12 +7,12 @@ and makes it easier to import into App.js.
 Note: A "barrel" file is a way to rollup exports from other modules into a single convenient module. 
 The "barrel" (module) file re-exports the exports of other modules.
 ================================================== */
-export { default as AllCampusesView } from "./AllCampusesView";
-export { default as AllStudentsView } from "./AllStudentsView";
-export { default as NewStudentView } from "./NewStudentView";
-export { default as CampusView } from "./CampusView";
-export { AddCampusView } from './AddCampusView';
-export { default as EditCampusView } from './EditCampusView';
-export { default as StudentView } from "./StudentView";
-export { default as EditStudentView } from './EditStudentView';
-export { default as HomePageView } from "./HomePageView";
+export { default as HomePageContainer } from "./HomePageContainer";
+export { default as AllCampusesContainer } from "./AllCampusesContainer";
+export { default as CampusContainer } from "./CampusContainer";
+export { default as AddCampusContainer } from './AddCampusContainer';
+export { default as EditCampusContainer } from './EditCampusContainer';
+export { default as AllStudentsContainer } from "./AllStudentsContainer";
+export { default as StudentContainer } from "./StudentContainer";
+export { default as EditStudentContainer } from './EditStudentContainer';
+export { default as NewStudentContainer } from "./NewStudentContainer";
