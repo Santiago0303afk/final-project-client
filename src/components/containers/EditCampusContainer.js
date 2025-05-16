43d37f67c7d3267790b185841f4525d23095e112
Santiago0303afk <santiago.cadme71@myhunter.cuny.edu>
2025-05-15 23:58:40 -0400

    Add files via upload

--- src/components/containers/EditCampusContainer.js
+++ src/components/containers/EditCampusContainer.js
@@ -1,37 +1,85 @@
-//SC CREATED THIS:
-import { Component } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchCampusThunk, editCampusThunk } from "../../store/thunks";
+import { fetchCampusThunk, updateCampusThunk } from "../../store/thunks";
 import EditCampusView from "../views/EditCampusView";
+import Header from "./Header";
 
 class EditCampusContainer extends Component {
-  componentDidMount() {
-    const campusId = this.props.match.params.id;
-    this.props.fetchCampus(campusId);
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      address: "",
+      description: "",
+      imageUrl: "",
+      errors: {}
+    };
   }
 
-  handleSubmit = (updatedCampus) => {
-    this.props.editCampus(updatedCampus);
-    window.location.href = `/campus/${updatedCampus.id}`;
+  async componentDidMount() {
+    const campusId = this.props.match.params.id;
+    await this.props.fetchCampus(campusId);
+    const { name, address, description, imageUrl } = this.props.campus;
+    this.setState({ name, address, description, imageUrl });
+  }
+
+  handleChange = (evt) => {
+    this.setState({ [evt.target.name]: evt.target.value });
+  };
+
+  validate = () => {
+    const errors = {};
+    if (!this.state.name.trim()) errors.name = "Name is required.";
+    if (!this.state.address.trim()) errors.address = "Address is required.";
+    return errors;
+  };
+
+  handleSubmit = async (evt) => {
+    evt.preventDefault();
+    const errors = this.validate();
+    if (Object.keys(errors).length > 0) {
+      this.setState({ errors });
+      return;
+    }
+
+    const updatedCampus = {
+      id: this.props.match.params.id,
+      name: this.state.name,
+      address: this.state.address,
+      description: this.state.description,
+      imageUrl: this.state.imageUrl
+    };
+
+    await this.props.updateCampus(updatedCampus);
+    this.props.history.push(`/campus/${updatedCampus.id}`);
   };
 
   render() {
     return (
-      <EditCampusView
-        campus={this.props.campus}
-        handleSubmit={this.handleSubmit}
-      />
+      <div>
+        <Header />
+        <EditCampusView
+          campus={this.props.campus}
+          name={this.state.name}
+          address={this.state.address}
+          description={this.state.description}
+          imageUrl={this.state.imageUrl}
+          errors={this.state.errors}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+      </div>
     );
   }
 }
 
 const mapState = (state) => ({
-  campus: state.campus, // Assuming you have a single campus reducer
+  campus: state.campus
 });
 
 const mapDispatch = (dispatch) => ({
   fetchCampus: (id) => dispatch(fetchCampusThunk(id)),
-  editCampus: (campus) => dispatch(editCampusThunk(campus)),
+  updateCampus: (campus) => dispatch(updateCampusThunk(campus))
 });
 
 export default connect(mapState, mapDispatch)(EditCampusContainer);
