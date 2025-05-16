2cd4c350a0a6a0791611648dd24ee6e0579fd805
Santiago0303afk <santiago.cadme71@myhunter.cuny.edu>
2025-05-16 12:39:30 -0400

    StudentContainer.js

--- src/components/containers/StudentContainer.js
+++ src/components/containers/StudentContainer.js
@@ -1,44 +1,27 @@
 import Header from './Header';
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchStudentThunk } from "../../store/thunks";
-import { StudentView } from "../views";
+import { fetchStudentThunk, deleteStudentThunk } from "../../store/thunks";
+import StudentView from "../views/StudentView";
 
 class StudentContainer extends Component {
   componentDidMount() {
-    const studentId = this.props.match?.params?.id;
-    if (studentId) {
-      this.props.fetchStudent(studentId);
-    } else {
-      console.error("No student ID found in route params.");
-    }
+    this.props.fetchStudent(this.props.match.params.id);
   }
 
+  handleDelete = async (studentId) => {
+    await this.props.deleteStudent(studentId);
+    this.props.history.push("/students");
+  };
+
   render() {
-    const { student } = this.props;
-
-    if (!student) {
-      return (
-        <div>
-          <Header />
-          <h2>Loading student...</h2>
-        </div>
-      );
-    }
-
-    if (!student.id) {
-      return (
-        <div>
-          <Header />
-          <h2>Student not found.</h2>
-        </div>
-      );
-    }
-
     return (
       <div>
         <Header />
-        <StudentView student={student} />
+        <StudentView
+          student={this.props.student}
+          handleDelete={this.handleDelete}
+        />
       </div>
     );
   }
@@ -53,6 +36,7 @@ const mapState = (state) => {
 const mapDispatch = (dispatch) => {
   return {
     fetchStudent: (id) => dispatch(fetchStudentThunk(id)),
+    deleteStudent: (id) => dispatch(deleteStudentThunk(id)),
   };
 };
 
