7290b6789a2c3a43e373facf7848c57dbd27a5b8
Santiago0303afk <santiago.cadme71@myhunter.cuny.edu>
2025-05-16 12:41:40 -0400

    campus.js

--- src/store/reducers/campus.js
+++ src/store/reducers/campus.js
@@ -2,10 +2,10 @@
 /src/store/reducers/campus.js
 
 This is a Reducer function that accepts 2 parameters: the previous state object (aka current state) and an action object. 
-Depending on the Action object, the Reducer updates the State and returns the new State object.
+Depending on the Action object, the Reducer updates the State and return the new State object.
 It also defines the State and its default initial value.
 ================================================== */
-import { FETCH_CAMPUS, EDIT_CAMPUS } from "../actions/actionTypes";  // Import Action Types
+import { FETCH_CAMPUS } from "../actions/actionTypes";  // Import Action Type
 
 // Define default Initial State
 const initialState = {
@@ -13,17 +13,12 @@ const initialState = {
 };
 
 // REDUCER:
-const campus = (state = initialState, action) => {
+const campus = (state = initialState, action) => {  // Use "initialState" as default Initial State
   switch (action.type) {
     case FETCH_CAMPUS:
       return action.payload;
-      
-    //SC ADDED 
-    case EDIT_CAMPUS:
-      return action.payload;  // Replace the current campus state with the updated campus
-
     default:
-      // If the Reducer doesn't recognize the Action Type, return the current State unchanged
+      // If the Reducer doesn't recognize the Action Type, returns the previous (current) State unchanged.
       return state;
   }
 };
