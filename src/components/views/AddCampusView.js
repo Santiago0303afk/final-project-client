3ded215ac5e6d1eb33ae0fa1d8c0b53c8644bd12
Santiago0303afk <santiago.cadme71@myhunter.cuny.edu>
2025-05-17 20:55:39 -0400

    AddCampusView.js

--- src/components/views/AddCampusView.js
+++ src/components/views/AddCampusView.js
@@ -1,44 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const AddCampusView = (props) => {
-  const {
-    name,
-    address,
-    description,
-    imageUrl,
-    errors,
-    handleChange,
-    handleSubmit
-  } = props;
-
-  return (
-    <div>
-      <h1>Add New Campus</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Name: </label>
-          <input name="name" value={name} onChange={handleChange} />
-          {errors.name && <p style={{ color: "red" }}>{errors.name}</p>}
-        </div>
-        <div>
-          <label>Address: </label>
-          <input name="address" value={address} onChange={handleChange} />
-          {errors.address && <p style={{ color: "red" }}>{errors.address}</p>}
-        </div>
-        <div>
-          <label>Description: </label>
-          <textarea name="description" value={description} onChange={handleChange} />
-        </div>
-        <div>
-          <label>Image URL: </label>
-          <input name="imageUrl" value={imageUrl} onChange={handleChange} />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
-};
+const AddCampusView = ({ name, address, description, imageUrl, errors, handleChange, handleSubmit }) => (
+  <div>
+    <h1>Add New Campus</h1>
+    <form onSubmit={handleSubmit}>
+      <input name="name" value={name} onChange={handleChange} placeholder="Name" />
+      {errors.name && <p>{errors.name}</p>}
+      
+      <input name="address" value={address} onChange={handleChange} placeholder="Address" />
+      {errors.address && <p>{errors.address}</p>}
+      
+      <textarea name="description" value={description} onChange={handleChange} placeholder="Description" />
+      <input name="imageUrl" value={imageUrl} onChange={handleChange} placeholder="Image URL" />
+      
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+);
 
 AddCampusView.propTypes = {
   name: PropTypes.string.isRequired,
